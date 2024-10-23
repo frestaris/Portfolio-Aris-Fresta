01133697f4444eab437ced8406226d92f6418534
Aris <frestaris@gmail.com>
2024-10-23 10:36:17 +1000

    outdir build

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,13 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     extensions: [".js", ".jsx"],
+  },
+  build: {
+    outDir: "build/client", // Specify the output directory for the build
   },
 });
